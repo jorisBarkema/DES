b09f3a4fd1e4f42c3ecd4102076b1dce9ea86f13
Joris Barkema <jorisbarkema@gmail.com>
2018-09-26 22:02:10 +0200

    key generation works
    I think

--- DES/DES/DES.cs
+++ DES/DES/DES.cs
@@ -11,8 +11,7 @@ namespace DES
         private List<long> message;
 
         private long fullKey;
-        private long permutatedKey;
-        private long[] keys;
+        private long[] keys = new long[16];
         private byte[] NumBitsToShiftKey = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
         public PermutationLibrary pl;
 
@@ -23,29 +22,31 @@ namespace DES
 
             this.pl = new PermutationLibrary();
 
-            Console.WriteLine("Applying key permutation to key");
-            this.permutatedKey = this.ApplyPermutation(k, this.pl.keyPermutation, true);
+            GenerateKeys();
+            
+            //Console.WriteLine("Applying expansion to first message block right half");
+            //this.ExpandedRightHalfOfMessage(message[0]);
 
-            Console.WriteLine("Applying expansion to first message block right half");
-            this.ExpandRightHalf(message[0]);
-
-            Console.WriteLine("Applying compression to permutated key");
-            this.CompressKey();
+            //Console.WriteLine("Applying compression to permutated key");
+            //this.CompressKey(permutatedKey);
         }
 
-        private void Encrypt()
+        private List<long> Encrypt()
         {
-            ApplyInitialPermutation();
+            for (int i = 0; i < message.Count; i++)
+            {
+                ApplyInitialPermutation(message[i]);
+            }
 
-            GenerateKeys();
+            return null;
         }
 
         /// <summary>
         /// Apply the initial permutation to a block of the message
         /// </summary>
-        public void ApplyInitialPermutation()
+        public long ApplyInitialPermutation(long l)
         {
-
+            return ApplyPermutation(l, pl.initialPermutation, true);
         }
 
         /// <summary>
@@ -53,20 +54,77 @@ namespace DES
         /// </summary>
         public void GenerateKeys()
         {
+            Console.WriteLine("Applying key permutation to key");
+            long permutatedKey = this.ApplyPermutation(fullKey, this.pl.keyPermutation, true);
+
+            Console.WriteLine();
+
+            long shiftedKey = permutatedKey, compressedKey;
+            for(int i = 0; i < 16; i++)
+            {
+                for(int j = 0; j < NumBitsToShiftKey[i]; j++)
+                {
+                    shiftedKey = ShiftKeyHalves(shiftedKey, false);
+                }
+                compressedKey = CompressKey(shiftedKey, false);
+                keys[i] = compressedKey;
+
+                Program.WriteLongAsBits(keys[i], "key number " + i);
+            }
+
+            Console.WriteLine();
 
         }
 
-        public void ExpandRightHalf(long l)
+        /// <summary>
+        /// Compresses the 56-bit key to a 48-bit key
+        /// </summary>
+        /// <param name="l"></param>
+        /// <returns></returns>
+        public long CompressKey(long l, bool debug = false) => ApplyPermutation(l, pl.compressionPermutation, debug);
+
+        /// <summary>
+        /// Shift the 2 halves of the 56-bit key by 1 bit
+        /// </summary>
+        /// <param name="l"> The key to be shifted</param>
+        /// <param name="debug">Whether or not debug info will be printed on the console</param>
+        /// <returns> Shifted key</returns>
+        public long ShiftKeyHalves(long l, bool debug = false)
+        {
+            if (debug) Program.WriteLongAsBits(l, "original key\t");
+            bool leftHead, rightHead;
+
+            //Look what the bit values in the heads are, which have to circle around
+            leftHead = (byte)((l >> 63) & 1) == 1;
+            // 63 - 28 = 35
+            rightHead = (byte)((l >> 35) & 1) == 1;
+
+            // Shift the key 1 bit to the left
+            l = l << 1;
+
+            // Set the tails to 0
+            // Since we cannot set them to 0 directly, we flip all the bits, then set them to 1 with bitwise or operator, then flip again
+            l = ~l;
+            l |= ((long)1 << 36);
+            l |= ((long)1 << 8);
+            l = ~l;
+
+            //if (debug) Program.WriteLongAsBits(l, "before added heads");
+
+            // Place the head values on the tail
+            if (leftHead) l |= (long)1 << 36;
+            if (rightHead) l |= (long)1 << 8;
+
+            if (debug) Program.WriteLongAsBits(l, "shifted key\t");
+            return l;
+        }
+
+        public long ExpandedRightHalfOfMessage(long l)
         {
             long r = Program.GetRightHalf(l);
-            r = ApplyPermutation(r, pl.expansionPermutation, true);
+            return ApplyPermutation(r, pl.expansionPermutation, true);
         }
-
-        public void CompressKey()
-        {
-            long ck = ApplyPermutation(permutatedKey, pl.compressionPermutation, true);
-        }
-
+        
         public long ApplyPermutation(long l, Permutation p, bool debug = false)
         {
             long res = 0;
@@ -75,7 +133,7 @@ namespace DES
 
             for (int i = 0; i < p.length; i++)
             {
-                res += ((l >> (63 - p.permutation[i])) & 1) << (63 - i);
+                res |= ((l >> (63 - p.permutation[i])) & 1) << (63 - i);
             }
 
             if (debug)
